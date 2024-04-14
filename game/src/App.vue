e4cbbe109afca83107cee2551e650dae65e30f41
Frank Dierolf <frank_dierolf@web.de>
2024-04-14 20:34:27 +0200

    :zap: (./game) add TresJS

--- game/src/App.vue
+++ game/src/App.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { TresCanvas } from "@tresjs/core";
+</script>
 
 <template>
   <main>
-    <h1>Hello Summar</h1>
+    <TresCanvas clear-color="#82DBC5" window-size>
+      <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" />
+      <TresMesh>
+        <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+        <TresMeshBasicMaterial color="orange" />
+      </TresMesh>
+      <TresAmbientLight :intensity="1" />
+    </TresCanvas>
   </main>
 </template>
 
-<style scoped></style>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
--- game/src/main.ts
+++ game/src/main.ts
@@ -2,5 +2,6 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import Tres from "@tresjs/core";
 
-createApp(App).mount("#app");
+createApp(App).use(Tres).mount("#app");
